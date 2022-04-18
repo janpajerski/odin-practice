a1cac10747f7f05e0c2d8570be524c089fae469a
Jan Pajerski <janpajerski@gmail.com>
2022-04-17 22:16:53 -0700

    Create function that takes input from the user
    Take input from a user and validate that it is a number. Then return the
    number.

--- javascript-practice/fizzbuzz.js
+++ javascript-practice/fizzbuzz.js
@@ -1,5 +1,10 @@
-function fizzBuzz () {
-    return userNum = prompt('Let\'s play FizzBuzz! Please enter a number');
+function getNumber() {
+    userNum = prompt('Let\'s play FizzBuzz! Please enter a number');
+    if (userNum === null){
+        return alert('I guess you don\'t want to play :(');
+    } else if (userNum === '' || isNaN(userNum * 1)) {
+        return alert('Enter a number if you want to play');
+    } else {return userNum);}
 }
 
-alert(fizzBuzz());
+getNumber();e

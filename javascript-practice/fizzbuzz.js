3b0af51549a4dc587d6c91583da2ae27f6dc37ad
Jan Pajerski <janpajerski@gmail.com>
2022-04-18 16:59:41 -0700

    Get FizzBuzz to run in console

--- javascript-practice/fizzbuzz.js
+++ javascript-practice/fizzbuzz.js
@@ -4,7 +4,22 @@ function getNumber() {
         return alert('I guess you don\'t want to play :(');
     } else if (userNum === '' || isNaN(userNum * 1)) {
         return alert('Enter a number if you want to play');
-    } else {return userNum);}
+    } else {return userNum;}
 }
 
-getNumber();e
+
+function  fizzBuzz(fizzNum){
+    for (i = 1; i <= fizzNum; i++) {
+        if (i % 3 == 0 && i % 5 == 0){
+            console.log('FizzBuzz');
+        } else if (i % 3 == 0){
+            console.log('Fizz');
+        } else if (i % 5 == 0){
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz(getNumber());

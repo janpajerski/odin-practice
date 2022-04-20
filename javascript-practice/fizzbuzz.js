d3dd42a12412df9bf498c530e2c81349bacce86b
Jan Pajerski <janpajerski@gmail.com>
2022-04-20 09:16:35 -0700

    Add defer attribute to script call in fizzbuzz
    I'm adding this so that my html elements load before the script is
    called. NOTE THAT (for me anyway) when the .js file was stored in the
    browser cache, it is executes immediately despite the defer attribute.
    As such, the HTML doesn't load until after the scrip is executed. It
    took me a lot of cycles to figure this out :)

--- javascript-practice/fizzbuzz.js
+++ javascript-practice/fizzbuzz.js
@@ -1,20 +1,22 @@
+fizzBuzz(getNumber());
+
 function getNumber() {
     userNum = prompt('Let\'s play FizzBuzz! Please enter a number');
     if (userNum === null){
         return alert('I guess you don\'t want to play :(');
     } else if (userNum === '' || isNaN(userNum * 1)) {
         return alert('Enter a number if you want to play');
-    } else {return userNum;}
+    } else {return parseInt(userNum);}
 }
 
 
 function  fizzBuzz(fizzNum){
     for (i = 1; i <= fizzNum; i++) {
-        if (i % 3 == 0 && i % 5 == 0){
+        if (i % 3 === 0 && i % 5 === 0){
             console.log('FizzBuzz');
-        } else if (i % 3 == 0){
+        } else if (i % 3 === 0){
             console.log('Fizz');
-        } else if (i % 5 == 0){
+        } else if (i % 5 === 0){
             console.log('Buzz');
         } else {
             console.log(i);
@@ -22,4 +24,3 @@ function  fizzBuzz(fizzNum){
     }
 }
 
-fizzBuzz(getNumber());
